1e2fafb372f5cd71cdf4669253b527949140a163
MaximTrofimenko <oklickk@yandex.ru>
2020-06-03 11:30:33 +0300

    реализация трехфазного конструктора

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -5,6 +5,5 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-
     }
 }
